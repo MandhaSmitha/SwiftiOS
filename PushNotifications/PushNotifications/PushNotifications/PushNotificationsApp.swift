bd69994540faf97044b2f99c1f5fa8c53713ed83
Mandha Smitha <smandhasmithas61@gmail.com>
2021-08-31 22:17:51 +0400

    Push Notifications
    Initial commit.

---
+++ PushNotifications/PushNotifications/PushNotifications/PushNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationsApp.swift
+//  PushNotifications
+//
+//  Created by Mandha Smitha S on 31/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
